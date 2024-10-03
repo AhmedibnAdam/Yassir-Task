b4fb908e873c697ee95e7b3a2e6b4f269442aae4
AhmedibnAdam <ibn.abuadam@gmail.com>
2024-10-03 20:19:14 +0300

    remove tests

---
+++ RickAndMortyTests/TestPresentationLayer/TestModuleScreens/TestViewModel/testCharctersListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  testCharctersListViewModel.swift
+//  RickAndMortyTests
+//
+//  Created by Ahmad on 03/10/2024.
+//
+
+import Testing
+
+struct testCharctersListViewModel {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
