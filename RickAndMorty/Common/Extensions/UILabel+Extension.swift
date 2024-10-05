d843abc317507b29646fdc219f06ac010a361db0
AhmedibnAdam <ibn.abuadam@gmail.com>
2024-10-05 05:10:27 +0300

    Enhancement

---
+++ RickAndMorty/Common/Extensions/UILabel+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UILabel+extension.swift
+//  RickAndMorty
+//
+//  Created by Ahmad on 05/10/2024.
+//
+
+extension UILabel {
+    func applyStyle(font: UIFont, color: UIColor) {
+        self.font = font
+        self.textColor = color
+    }
+}
